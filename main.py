a6295fa09a0cc83a8e46f1ec82e23389fd51376d
arvinhadidi <arvinhadidifard@gmail.com>
2025-05-06 11:56:16 +0100

    assigning players to teams
    - k-means clustering is used on a screenshot of a player to find their shirt colour
    - the shirt colours are then used in the visualisation on the output video

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
+from team_assigner import TeamAssigner
 
 def main():
     # read video
@@ -11,9 +13,34 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path = "stubs/track_stubs.pkl")
-    output_video_frames = tracker.draw_annotations(video_frames, tracks)
-    print(f"Total frames tracked: {len(tracks['players'])}")
 
+    # print(f"Total frames tracked: {len(tracks['players'])}")
+
+    # save cropped image of one player
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player['bbox']
+    #     frame = video_frames[0]
+
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+    #     cv2.imwrite(f"output_videos/cropped_image.jpg", cropped_image)
+
+    #     break
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    # for each frame, assign each player to a team
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track['bbox'], player_id)
+
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
     # save video
     save_video(output_video_frames, "output_videos/short_output_vid.avi")
